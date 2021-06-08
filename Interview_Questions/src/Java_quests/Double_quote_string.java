f8fb1d356675d62c94072aec5d594ed7a17590ba
Priya141092 <priyasaxena1492@gmail.com>
2021-06-08 19:27:58 +0530

    My new commit

---
+++ Interview_Questions/src/Java_quests/Double_quote_string.java
@@ -0,0 +1,41 @@
+package Java_quests;
+
+public class Double_quote_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String m =  " \" OOPS \" testsnew\"restcs\"";
+		
+		
+			String k[]=m.split(""); 
+				
+			for(int i=0;i<k.length;i++)
+			{
+				//System.out.println(k[i]);
+				
+				if(k[i].matches(""))
+				{
+					String p = k[i];
+					System.out.println(p);
+				}
+			}
+			
+		
+		
+
+//      String[] k = m.split("\"");
+//      
+//      for(int i=0;i<k.length;i++)
+//      
+//      {
+//      if(k[i].matches(""))
+//      {
+//    	  l=k[i];
+//    	  
+//      }
+      
+	}
+	}
+
+
